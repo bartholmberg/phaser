a1677ef21c14d9ad7b0f958e023351e292bf5f89
bart holmberg <bart.holmberg@gmail.com>
2022-08-27 14:12:41 -0700

    make the junction to binding

--- startup.cmd
+++ startup.cmd
@@ -1,16 +1,15 @@
 @echo off
 chcp 65001
-set junctionDir=pntphr.jnc
-set phaserRepoDir=c:\repo\phaser
+set junctionDir=apricus.jnc
+set phaserRepoDir=c:\repo\apricus
 if not exist %phaserRepoDir% (
    echo you need to enlist %phaserRepoDir%  
    exit /b
 )
 IF not exist %junctionDir% ( 
 	echo ........⏱️ 
-	..\apricus\tool\junction -s -q -nobanner %junctionDir%  %phaserRepoDir%  
+	..\phaser\tool\junction -s -q -nobanner %junctionDir%  %phaserRepoDir%  
 	rem and if you want to delete junction
 	rem ..\apricus\tool\junction -d %junctionDir% 
 	echo ⚽ All Done ✨  
 	exit /b ) 
-
